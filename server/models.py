3faeda0e30204af2f4e130208588d730aa218307
marie <mariegacheri@gmail.com>
2024-02-06 11:44:51 +0300

    patent model

--- server/models.py
+++ server/models.py
@@ -2,3 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+class Patent(db.model):
+    __tablename__= 'patents'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(400), nullable=False)
+    patent_status = db.Column(db.String(50))
+    summary= db.Column(db.String)
+    created_at=db.Column(db.DateTime(), server_default=db.func.now())
+    updated_at=db.Column(db.DateTime(), onupdate=db.func.now())
+    
