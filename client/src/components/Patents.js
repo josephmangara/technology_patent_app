50e5e687a388a34949519d4d1c33a5e31ae1c2f8
josephmangara <josephmangara2@gmail.com>
2024-03-03 16:05:46 +0300

    updates

--- client/src/components/Patents.js
+++ client/src/components/Patents.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Patent() {
   const [patents, setPatents] = useState([]);
@@ -48,7 +49,8 @@ export default function Patent() {
         />
         <button className="button">🔍</button>
       </div>
-      <div id="patents-page">
+      <div className="page-look">
+      <div id="patents-page" className="col-md-6">
         <h2 id="patents">Patents</h2>
         <ol id="lists">
           {searchResults.map((patent, id) => (
@@ -60,6 +62,15 @@ export default function Patent() {
             </li>
           ))}
         </ol>
+      </div>
+        <div id="right-col" className="col-md-6">
+          <h3>What is a patent?</h3>
+          <img id="image" src="https://live.staticflickr.com/540/19603716723_6d04114159_z.jpg" alt="definition of a patent" />
+          <p id="patent-def">A patent is a form of intellectual property right granted by the government to an inventor or assignee, giving them the exclusive right to make, use, and sell their invention for a limited period of time. 
+            Patents are granted for inventions that are novel, non-obvious, and useful. They provide inventors with the incentive to invest time and resources into researching and developing new technologies or products 
+            by granting them a period of exclusivity during which they can profit from their invention without competition. In exchange for this exclusivity, the inventor is required to disclose the details of their 
+            invention in the patent application, which contributes to the body of public knowledge.</p>
+        </div>
       </div>
       <Footer />
     </>
